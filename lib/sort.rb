cb2b3440e709dc320f1ee1fa9195b38b3d54321c
Jenni Choi <jenni0729@gmail.com>
2015-03-17 09:35:38 -0700

    homework

--- lib/sort.rb
+++ lib/sort.rb
@@ -1,6 +1,26 @@
+bubbleSort(array);
+
+Paris [10:47 PM]
 class Array
 
-  def bubble_sort
-  end
-
+ def bubble_sort
+     i = 0
+     while i < self.length do
+         j = i + 1
+            while j < self.length do
+                if self[i] > self[j]
+                    temp = self[i]
+                    self[i] = self[j]
+                    self[j] = temp
+                end
+                j += 1
+            end
+         i += 1
+     end
+     self
+ end
 end
+
+
+
+
